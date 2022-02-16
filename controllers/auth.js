9b45287ff95c8a5739dad8b8dea5feee4d985be6
mohamedisakr <mohamedisakr@gmail.com>
2022-02-16 14:41:23 +0200

    sending email on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,54 @@
 const User = require('../models/user')
+const {sign} = require('jsonwebtoken')
+const sendgridMail = require('@sendgrid/mail')
+
+const {
+    SENDGRID_API_KEY,
+    JWT_ACCOUNT_ACTIVATION,
+    JWT_EXPIRE_IN,
+    EMAIL_FROM,
+    CLIENT_URL,
+} = process.env
+
+sendgridMail.setApiKey(SENDGRID_API_KEY)
+
+exports.signup = async (req, res) => {
+    const {name, email, password} = req.body
+    const foundByEmail = await User.findOne({email}).exec()
+
+    if (foundByEmail) {
+        return res.status(400).json({message: 'EMail already exists'})
+    }
+
+    const token = sign({name, email, password}, JWT_ACCOUNT_ACTIVATION, {
+        expiresIn: JWT_EXPIRE_IN,
+    })
+
+    const emailMessage = {
+        from: EMAIL_FROM,
+        to: email,
+        subject: `Activate your account`,
+        html: `
+            <h2>Please use the following link to activate your account</h2>
+            <p>${CLIENT_URL}/auth/activate/${token}</p>
+            <hr/>
+            <p>This email may contain sensetive information</p>
+        `,
+    }
+
+    sendgridMail
+        .send(emailMessage)
+        .then((sent) => {
+            // console.log(sent)
+            return res.json({
+                message: `Confirmation email has been sent to ${email}. Follow the instructions to activate your account`,
+            }).cat
+        })
+        .catch((err) => {
+            // console.log(err)
+            return res.json({message: err.message})
+        })
+}
 
 // exports.signup = async (req, res) => {
 //     console.log(`req.body ${JSON.stringify(req.body)}`)
@@ -18,22 +68,3 @@ const User = require('../models/user')
 //         return res.status(201).json({message: 'Signup success, please signin'})
 //     })
 // }
-
-exports.signup = async (req, res) => {
-    console.log(`req.body ${JSON.stringify(req.body)}`)
-    const {name, email, password} = req.body
-    const foundByEmail = await User.findOne({email}).exec()
-    if (foundByEmail) {
-        return res.status(400).json({message: 'EMail already exists'})
-    }
-
-    const userToAdd = new User({name, email, password}) //await User.create({name, email, password})
-    userToAdd.save((err, success) => {
-        if (err) {
-            console.log(`signup errer: ${err}`)
-            return res.status(400).json({message: 'Signup errer'})
-        }
-
-        return res.status(201).json({message: 'Signup success, please signin'})
-    })
-}
---
+++ fixtures/user-data.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        name: 'mern sec',
+        email: 'mern-sec@outlook.com',
+        password: 'Password123',
+    },
+    {
+        name: 'moha sakr',
+        email: 'mohamedisakr@gmail.com',
+        password: 'Password123',
+    },
+]
