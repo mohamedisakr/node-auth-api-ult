8bcc15c47a2a8a25b89d71911801242e11b27639
mohamedisakr <mohamedisakr@gmail.com>
2022-02-16 11:57:27 +0200

    updating signup controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
 const User = require('../models/user')
 
+// exports.signup = async (req, res) => {
+//     console.log(`req.body ${JSON.stringify(req.body)}`)
+//     const {name, email, password} = req.body
+//     const foundByEmail = await User.findOne({email}).exec()
+//     if (foundByEmail) {
+//         return res.status(400).json({message: 'EMail already exists'})
+//     }
+
+//     const userToAdd = new User({name, email, password}) //await User.create({name, email, password})
+//     userToAdd.save((err, success) => {
+//         if (err) {
+//             console.log(`signup errer: ${err}`)
+//             return res.status(400).json({message: 'Signup errer'})
+//         }
+
+//         return res.status(201).json({message: 'Signup success, please signin'})
+//     })
+// }
+
 exports.signup = async (req, res) => {
     console.log(`req.body ${JSON.stringify(req.body)}`)
     const {name, email, password} = req.body
