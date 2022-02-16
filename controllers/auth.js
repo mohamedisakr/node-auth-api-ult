c883739903eb18e1c7c3e635c9519e5ebe80d08b
mohamedisakr <mohamedisakr@gmail.com>
2022-02-16 10:38:06 +0200

    implementing auth validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
 exports.signup = (req, res) => {
+  console.log(`req.body ${JSON.stringify(req.body)}`)
   res.json({data: 'you hit signup endpoint'})
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const {signup} = require('../controllers/auth')
+const {runValidation} = require('../validators')
+const {userSignupValidator} = require('../validators/auth')
 const router = express.Router()
 
-router.get('/signup', signup)
+// router.get('/signup', signup)
+router.post('/signup', userSignupValidator, runValidation, signup)
 
 module.exports = router
