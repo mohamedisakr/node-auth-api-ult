2f55549d25214949e6a98056f90e2ab8ef46475c
mohamedisakr <mohamedisakr@gmail.com>
2022-02-19 09:50:19 +0200

    implementing update user endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,50 @@ exports.findOneById = (req, res) => {
       res.status(200).json(user)
     })
 }
+
+exports.update = (req, res) => {
+  //   console.log(`req.user ${JSON.stringify(req.user)}`)
+  //   console.log(`req.body ${JSON.stringify(req.body)}`)
+  const {_id} = req.user
+  const {name, password} = req.body
+  User.findById({_id}, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({message: 'User not found'})
+    }
+
+    if (!name) {
+      return res.status(400).json({message: 'Name is required'})
+    } else {
+      user.name = name
+    }
+
+    if (password) {
+      if (password.length < 6) {
+        return res
+          .status(400)
+          .json({message: 'Password must be at least 6 characters long'})
+      } else {
+        user.password = password
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err || !updatedUser) {
+        return res.status(400).json({message: 'User update failed'})
+      }
+
+      updatedUser.hashed_password = undefined
+      updatedUser.salt = undefined
+      res.status(200).json(updatedUser)
+    })
+  })
+
+  //   User.findById(id)
+  //     .select('-hashed_password -salt')
+  //     .exec((err, user) => {
+  //       if (err || !user) {
+  //         return res.status(400).json({message: 'User not found'})
+  //       }
+  //       res.status(200).json(user)
+  //     })
+}
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {requireSignin} = require('../controllers/auth')
-const {findOneById} = require('../controllers/user')
+const {findOneById, update} = require('../controllers/user')
 const {runValidation} = require('../validators')
 const {userSignupValidator, userSigninValidator} = require('../validators/auth')
 const router = express.Router()
 
 router.get('/user/:id', requireSignin, findOneById)
+router.put('/user/update', requireSignin, update)
 
 module.exports = router
